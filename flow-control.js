207590f17522bd6cfa3e526b1cadcffed4686b17
Asa Tirsen <asa.tirsen@gmail.com>
2019-05-19 23:25:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,8 +24,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  age >= 13 && age <= 19 ? "You are a teenager!" :
-  "You are not a teenager"
+  return age >= 13 && age <= 19 ? "You are a teenager!" : "You are not a teenager"
 }
 
 function switchAge(age) {
@@ -45,10 +44,10 @@ function switchAge(age) {
    case age = 17:
    console.log("You are a teenager");
    break;
-   case age = 17:
+   case age = 18:
    console.log("You are a teenager");
    break;
-   case age = 17:
+   case age = 19:
    console.log("You are a teenager");
    break;
    default:
